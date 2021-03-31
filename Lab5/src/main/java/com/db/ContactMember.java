952eedebeaae74023768ecdf237f12207fa165d2
mcam24mcam26 <80918603+mcam24mcam26@users.noreply.github.com>
2021-03-31 20:15:41 +0530

    Add files via upload

---
+++ Lab5/src/main/java/com/db/ContactMember.java
@@ -0,0 +1,41 @@
+package com.db;
+
+public class ContactMember {
+	private String cname,cmob,cmail;
+
+	public ContactMember(String cname, String cmob, String cmail) {
+		super();
+		this.cname = cname;
+		this.cmob = cmob;
+		this.cmail = cmail;
+	}
+
+	public ContactMember() {
+		super();
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public String getCmob() {
+		return cmob;
+	}
+
+	public void setCmob(String cmob) {
+		this.cmob = cmob;
+	}
+
+	public String getCmail() {
+		return cmail;
+	}
+
+	public void setCmail(String cmail) {
+		this.cmail = cmail;
+	}
+	
+}
